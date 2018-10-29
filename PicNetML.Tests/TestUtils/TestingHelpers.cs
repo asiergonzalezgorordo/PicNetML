a6a455d3c2e61918464888ebe992b489e8de7c16
Asier Gonzalez Gorordo <agonzalezgorordo@azterlan.es>
2018-10-29 08:44:18 +0100

    NUnit updated to version 3

--- PicNetML.Tests/TestUtils/TestingHelpers.cs
+++ PicNetML.Tests/TestUtils/TestingHelpers.cs
@@ -1,23 +1,30 @@
-﻿using System.IO;
+﻿using NUnit.Framework;
+using System.IO;
 using System.Linq;
 
-namespace PicNetML.Tests.TestUtils {
-  public static class TestingHelpers {
+namespace PicNetML.Tests.TestUtils
+{
+	public static class TestingHelpers
+	{
 
-    public static string GetResourceFileName(string filename) {
-      return @"..\..\resources\" + filename;
-    }
+		public static string GetResourceFileName(string filename)
+		{
+			var dir = TestContext.CurrentContext.TestDirectory;
+			return $@"{dir}\..\..\resources\{filename}";
+		}
 
-    public static Runtime LoadSmallRuntime<T>(string filename, int classidx, int count) where T : new() {
-      var file = GetResourceFileName(filename);
-      using (var fs = File.OpenText(file)) {
-        string[] lines = Enumerable.Range(0, count + 1).          
-          Select(i => fs.ReadLine()).
-          Skip(1).
-          ToArray();
-        var rows = Runtime.LoadRowsFromLines<T>(lines);
-        return Runtime.LoadFromRows(classidx, rows);
-      }            
-    }
-  }
+		public static Runtime LoadSmallRuntime<T>(string filename, int classidx, int count) where T : new()
+		{
+			string file = GetResourceFileName(filename);
+			using (StreamReader fs = File.OpenText(file))
+			{
+				string[] lines = Enumerable.Range(0, count + 1).
+					Select(i => fs.ReadLine()).
+					Skip(1).
+					ToArray();
+				System.Collections.Generic.IEnumerable<T> rows = Runtime.LoadRowsFromLines<T>(lines);
+				return Runtime.LoadFromRows(classidx, rows);
+			}
+		}
+	}
 }
