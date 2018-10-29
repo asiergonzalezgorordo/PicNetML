4b8f787ee3b3d23830aa41cbfafb896319da2777
Asier Gonzalez Gorordo <agonzalezgorordo@azterlan.es>
2018-10-29 10:29:49 +0100

    Weka dll generated with version 3.7.11.0

--- build_weka_dlls.bat
+++ build_weka_dlls.bat
@@ -6,4 +6,4 @@ REM DOWNLOAD WEKA PACKAGES FROM: http://sourceforge.net/projects/weka/files/weka
 cd D:\git\PicNetML
 lib\ikvm-7.2.4630.5\bin\ikvmc -debug -target:library^
  -classloader:ikvm.runtime.ClassPathAssemblyClassLoader^
- lib\weka\weka.jar lib\weka\packages\*.jar -out:lib\weka.dll
+ lib\weka\weka.jar lib\weka\packages\*.jar -out:lib\weka.dll -version:3.7.11.0 
