9825446b1151a4b9b5696bcbd6d7a13b8ddebc71
Asier Gonzalez Gorordo <agonzalezgorordo@azterlan.es>
2018-10-29 09:15:35 +0100

    Bat fixed

--- build_weka_dlls.bat
+++ build_weka_dlls.bat
@@ -1,8 +1,9 @@
-ECHO OFF
+REM ECHO OFF
 
 REM DOWNLOAD WEKA FROM: http://www.cs.waikato.ac.nz/ml/weka/snapshots/developer-branch.zip
 REM DOWNLOAD WEKA PACKAGES FROM: http://sourceforge.net/projects/weka/files/weka-packages/
 
-%cd:~0,3%\dev\tools\ikvm\bin\ikvmc -debug -target:library^
+cd D:\git\PicNetML
+lib\ikvm-7.2.4630.5\bin\ikvmc -debug -target:library^
  -classloader:ikvm.runtime.ClassPathAssemblyClassLoader^
  lib\weka\weka.jar lib\weka\packages\*.jar -out:lib\weka.dll
