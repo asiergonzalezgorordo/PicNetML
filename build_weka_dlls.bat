7a41387af2f285eb92a05fafb78141d12facb8c4
Asier Gonzalez Gorordo <agonzalezgorordo@azterlan.es>
2018-10-29 11:06:35 +0100

    Weka dll updated

--- build_weka_dlls.bat
+++ build_weka_dlls.bat
@@ -4,6 +4,6 @@ REM DOWNLOAD WEKA FROM: http://www.cs.waikato.ac.nz/ml/weka/snapshots/developer-
 REM DOWNLOAD WEKA PACKAGES FROM: http://sourceforge.net/projects/weka/files/weka-packages/
 
 cd D:\git\PicNetML
-lib\ikvm-7.2.4630.5\bin\ikvmc -debug -target:library^
+lib\ikvm-7.2.4630.5\bin\ikvmc -target:library^
  -classloader:ikvm.runtime.ClassPathAssemblyClassLoader^
  lib\weka\weka.jar lib\weka\packages\*.jar -out:lib\weka.dll -version:3.7.11.0 
